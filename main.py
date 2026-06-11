8244f058d169100c7ba76a82a20fd67b3a278043
Dongmin Lee <fftl0785@gmail.com>
2026-06-12 04:22:53 +0900

    feat: CGP 서비스계정 BigQuery에 연결 설정

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import feedparser
+from dotenv import load_dotenv
+from google.cloud import bigquery
+
+#GCP 서비스 연결 확인
+load_dotenv()
+client = bigquery.Client()
+print("연결 성공! 프로젝트:", client.project)
 
 # 뽐뿌 핫딜 RSS (기존에 쓰던 실제 URL로 교체)
 url = "https://www.ppomppu.co.kr/rss.php?id=ppomppu"
